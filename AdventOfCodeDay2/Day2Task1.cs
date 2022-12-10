925f7ed4aae9d8ee42fc07c7b92187eed495e8b6
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-10 14:43:19 +0100

    Add files via upload

---
+++ AdventOfCodeDay2/Day2Task1.cs
@@ -0,0 +1,18 @@
+﻿int Score=0;
+Dictionary<char, int> ScoreMap = new Dictionary<char, int>() { { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
+
+foreach (string line in File.ReadLines(@"C:\\Day2Input.txt"))
+{
+    int Opponent = ScoreMap[line[0]];
+    int YourMove = ScoreMap[line[2]];
+    //Score for Shape
+    Score += YourMove;
+    //Score for Win
+    int WhoWon = YourMove - Opponent; //-2 1 1 Win 2 5 -1 Lose
+    Score += ((WhoWon == 1) || (WhoWon == -2) ? 6 : 0);
+    //Score for Draw
+    Score += (YourMove == Opponent ? 3 : 0);
+
+}
+Console.WriteLine(Score);
+Console.ReadLine();
---
+++ AdventOfCodeDay2/Day2Task2.cs
@@ -0,0 +1,28 @@
+﻿int Score=0;
+Dictionary<char, int> ScoreMap = new Dictionary<char, int>() { {'X',1}, { 'Y', 2 }, { 'Z', 3 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
+
+foreach (string line in File.ReadLines(@"C:\\Day2Input.txt"))
+{
+    int Opponent = ScoreMap[line[0]];
+    int YourMove = ScoreMap[line[2]];
+    //Score for Shape
+    if (YourMove == 1)
+    {
+        Score+=(Opponent == 1 ? 3 : (Opponent-1));
+        //Lose no points
+    }
+    else if (YourMove == 2)
+    {
+        Score+=Opponent;
+        Score += 3; //Draw
+    }
+    else if (YourMove==3)
+    {
+        Score+=(Opponent==3 ? 1 : (Opponent+1));
+        Score += 6;//Win
+    }
+
+}
+Console.WriteLine(Score);
+Console.ReadLine();
+
