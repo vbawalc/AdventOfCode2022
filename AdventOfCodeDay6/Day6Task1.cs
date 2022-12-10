40d6e922a2ce2d1215c6c626e558e2a545e484cf
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-10 15:05:42 +0100

    Day 6 added to collective repository

---
+++ AdventOfCodeDay6/Day6Task1.cs
@@ -0,0 +1,20 @@
+﻿char[] input = File.ReadAllText(@"C:\\Day6Input.txt").ToArray();
+List<char> marker = new List<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (marker.Count == 4)
+    {
+        marker.RemoveAt(0);
+        marker.Add(input[i]);
+        if (marker.Count() == marker.Distinct().Count())
+        {
+            Console.WriteLine(i + 1);
+            break;
+        }
+    }
+    else
+    {
+        marker.Add(input[i]);
+    }
+}
