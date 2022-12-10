d1dce48c64fb54178bc051a4a1308ba8945152f3
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-10 14:55:37 +0100

    Added day4 to collective repository.

---
+++ AdventOfCodeDay4/Day4Task1.cs
@@ -0,0 +1,17 @@
+﻿int Score = 0;
+
+foreach (string line in File.ReadLines(@"C:\\Day4Input.txt"))
+{
+    string[] subs = line.Split('-',',');
+    // first Elf assignments fully contain 2nd elf assignments
+    if (int.Parse(subs[0]) <= int.Parse(subs[2]) && int.Parse(subs[1]) >= int.Parse(subs[3]))
+    {
+        Score++;
+    }
+    // 2nd Elf assignments fully contain 1st elf assignment
+    else if (int.Parse(subs[2]) <= int.Parse(subs[0]) && int.Parse(subs[3]) >= int.Parse(subs[1]))
+    {
+        Score++;
+    }
+}
+Console.WriteLine(Score);
---
+++ AdventOfCodeDay4/Day4Task2.cs
@@ -0,0 +1,33 @@
+﻿int Score = 0;
+int LinesCount = 0;
+foreach (string line in File.ReadLines(@"C:\\Users\\BartłomiejWalczak\Desktop\Elves.txt"))
+{
+    string[] subs = line.Split('-', ',');
+    if (int.Parse(subs[1]) < int.Parse(subs[2]) && int.Parse(subs[0]) < int.Parse(subs[3]))
+    {
+        Score++;
+    }
+    else if (int.Parse(subs[1]) > int.Parse(subs[2]) && int.Parse(subs[0]) > int.Parse(subs[3]))
+        {
+        Score++;
+    }
+    LinesCount++;
+}
+Console.WriteLine(LinesCount-Score);
+
+/*foreach (string line in File.ReadLines(@"C:\\Users\\BartłomiejWalczak\Desktop\Elves.txt"))
+{
+    string[] subs = line.Split('-',',');
+    // first Elf assignments fully contain 2nd elf assignments
+    if (int.Parse(subs[0]) <= int.Parse(subs[2]) && int.Parse(subs[1]) >= int.Parse(subs[3]))
+    {
+        Score++;
+    }
+    // 2nd Elf assignments fully contain 1st elf assignment
+    else if (int.Parse(subs[2]) <= int.Parse(subs[0]) && int.Parse(subs[3]) >= int.Parse(subs[1]))
+    {
+        Score++;
+    }
+}
+Console.WriteLine(Score);*/
+
