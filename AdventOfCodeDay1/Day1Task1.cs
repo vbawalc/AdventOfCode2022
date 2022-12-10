97b901fd2961e2df75200eae6a66259446e2520a
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-10 14:28:36 +0100

    Add files via upload

---
+++ AdventOfCodeDay1/Day1Task1.cs
@@ -0,0 +1,16 @@
+﻿int MaximumElf = 0;
+int CurrentElf = 0;
+
+foreach (string line in File.ReadLines(@"C:\\Users\\BartłomiejWalczak\Desktop\Elves.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        MaximumElf = (CurrentElf > MaximumElf ? CurrentElf : MaximumElf);
+        CurrentElf = 0;
+    }
+    else
+        CurrentElf += int.Parse(line);
+}
+MaximumElf = (CurrentElf > MaximumElf ? CurrentElf : MaximumElf);
+Console.WriteLine(MaximumElf);
+Console.ReadLine();
---
+++ AdventOfCodeDay1/Day1Task2.cs
@@ -0,0 +1,19 @@
+﻿int[] MaximumElves = new int[3];
+int CurrentElf =0;
+
+foreach (string line in File.ReadLines(@"C:\\Users\\BartłomiejWalczak\Desktop\Elves.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        Array.Sort(MaximumElves);
+        MaximumElves[0] = (CurrentElf > MaximumElves[0] ? CurrentElf : MaximumElves[0]);
+        CurrentElf = 0;
+    }
+    else
+        CurrentElf += int.Parse(line);
+}
+Array.Sort(MaximumElves);
+MaximumElves[0] = (CurrentElf > MaximumElves[0] ? CurrentElf : MaximumElves[0]);
+Console.WriteLine(MaximumElves[0] + MaximumElves[1] + MaximumElves[2]);
+Console.ReadLine();
+
