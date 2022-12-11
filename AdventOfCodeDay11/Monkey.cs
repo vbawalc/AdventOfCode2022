ece42b06822e5fe935957f948f32e3a0ea39e06a
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-11 20:59:18 +0100

    Added Part 1

---
+++ AdventOfCodeDay11/Monkey.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCodeDay11
+{
+    internal class Monkey
+    {
+        public List<int> items;
+        public int inspecteditems;
+
+        private string operation;
+        private char operationSign;
+        private int test;
+        private int throwwhentrue;
+        private int throwwhenfalse;
+       
+
+        public Monkey()
+        {
+            items = new List<int>();
+            inspecteditems = 0;
+        }
+        public string Operation => operation;
+        public char OperationSign => operationSign;
+        public int Test => test;
+        public int Throwwhentrue => throwwhentrue;
+        public int Throwwhenfalse => throwwhenfalse;
+
+        public void AddItem(int item)
+        {
+            items.Add(item);
+        }
+        public void setOperation(string _operation)
+        {
+            operation = _operation;
+        }
+        public void setOperationSign(char _operationSign)
+        {
+            operationSign = _operationSign;
+        }
+        public void setTest(int _test)
+        {
+            test = _test;
+        }
+        public void setThrowTrue(int _throwwhentrue)
+        {
+            throwwhentrue = _throwwhentrue;
+        }
+        public void setThrowFalse(int _throwwhenfalse)
+        {
+            throwwhenfalse = _throwwhenfalse;
+        }
+    }
+}
---
+++ AdventOfCodeDay11/Program.cs
@@ -0,0 +1,109 @@
+﻿using AdventOfCodeDay11;
+using System.Data.SqlTypes;
+
+List <Monkey> monkeys = new List <Monkey> ();
+List<int> monkeybussiness = new List<int>();
+
+LoadMonkeys();
+for(int i=0;i<20;i++)
+{
+    for (int j=0;j<monkeys.Count;j++)
+    {
+        //CHECK IF MONKEY HOLDS ANY ITEMS
+        if (monkeys[j].items.Count > 0)
+        {
+            //THROW ALL ITEMS 1 BY 1
+            for (int k=0;k < monkeys[j].items.Count;k++)
+            {
+                monkeys[j].inspecteditems++;
+                //INSPECT ITEM
+                int worrylevel = monkeys[j].items[k];
+                Console.WriteLine("Inspecting item:" + worrylevel);
+                if (monkeys[j].OperationSign == '+')
+                {
+                    worrylevel += int.Parse(monkeys[j].Operation);
+                }
+                else
+                {
+                    if (monkeys[j].Operation == "old")
+                    {
+                        worrylevel = worrylevel * worrylevel;
+                    }
+                    else
+                    {
+                        worrylevel = worrylevel * int.Parse(monkeys[j].Operation);
+                    }
+                }
+                //GET BORED WITH ITEM 
+                worrylevel = worrylevel / 3;
+                //DECIDE WHERE TO THROW AN ITEM
+                if (worrylevel % monkeys[j].Test == 0)
+                {
+                    monkeys[monkeys[j].Throwwhentrue].AddItem(worrylevel);
+                }
+                else
+                {
+                    monkeys[monkeys[j].Throwwhenfalse].AddItem(worrylevel);
+                }
+            }
+            monkeys[j].items.Clear();
+            
+        }
+    }
+}
+
+//FIND MONKEYS THAT INSPECTED MOST ITEMS
+foreach(Monkey monkey in monkeys)
+{
+    monkeybussiness.Add(monkey.inspecteditems);
+}
+monkeybussiness.Sort();
+int result = monkeybussiness[monkeybussiness.Count - 1] * monkeybussiness[monkeybussiness.Count - 2];
+Console.WriteLine(result);
+
+
+
+
+void LoadMonkeys()
+{
+    int currentmonkey = -1;
+    string[] lines = File.ReadLines(@"\\Day11Input.txt").ToArray();
+    for (int i = 0; i < lines.Length; i++)
+    {
+        lines[i] = lines[i].Trim();
+        string[] line = lines[i].Split(' ', ',');
+        if (line[0] == "Monkey")
+        {
+            currentmonkey++;
+            monkeys.Add(new Monkey());
+        }
+        else if (line[0] == "Starting")
+        {
+            for (int j = 2; j < line.Length; j += 2)
+            {
+                monkeys[currentmonkey].AddItem(int.Parse(line[j]));
+            }
+        }
+        else if (line[0] == "Operation:")
+        {
+            monkeys[currentmonkey].setOperationSign(char.Parse(line[4]));
+            monkeys[currentmonkey].setOperation(line[5]);
+        }
+        else if (line[0] == "Test:")
+        {
+            monkeys[currentmonkey].setTest(int.Parse(line[3]));
+        }
+        else if (line[0] == "If")
+        {
+            if (line[1] == "true:")
+            {
+                monkeys[currentmonkey].setThrowTrue(int.Parse(line[5]));
+
+            }
+            else
+            {
+                monkeys[currentmonkey].setThrowFalse(int.Parse(line[5]));
+            }
+        }
+    }
+}
