bc1b15a903975064fc48533a7d0d3249aec75045
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-15 20:24:05 +0100

    1st Task added

---
+++ AdventOfCodeDay13/PacketValue.cs
@@ -0,0 +1,125 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace AdventOfCodeDay13;
+
+public class PacketValues
+{
+    public JsonElement packets;
+    public readonly int index;
+
+    public PacketValues(int index, JsonElement newPair)
+    {
+        this.index = index;
+        packets = newPair;
+    }
+    public static void CreateList(string filename, List<PacketValues> leftPacketValues, List<PacketValues> rightPacketValues)
+    {
+        var lines = File.ReadAllLines(filename);
+
+        var left = true;
+        var i = 1;
+
+        foreach (var line in lines)
+        {
+            if (line.Length == 0)
+            {
+                continue;
+            }
+
+            var jason = JsonSerializer.Deserialize<JsonElement>(line); //CREATE OBJECT FROM JSON
+
+            var newPair = new PacketValues(i, jason);
+
+            if (left)
+            {
+                leftPacketValues.Add(newPair);
+            }
+            else
+            {
+                rightPacketValues.Add(newPair); ;
+
+                i++;
+            }
+
+            left = !left;
+        }
+    }
+
+    public int Compare(JsonElement leftPair, JsonElement rightPair)
+    {
+        JsonElement newPacketValues;
+
+        switch (leftPair.ValueKind, rightPair.ValueKind)
+        {
+            //1st list runs out of items
+            case (JsonValueKind.Null, not JsonValueKind.Null):
+                return -1;
+            //2nd list runs out of items
+            case (not JsonValueKind.Null, JsonValueKind.Null):
+                return 1;
+            // Both items are ints
+            case (JsonValueKind.Number, JsonValueKind.Number):
+                return Comparer<int>.Default.Compare(leftPair.GetInt32(), rightPair.GetInt32());
+            //Firsr item is int 2nd item is List
+            case (JsonValueKind.Number, JsonValueKind.Array):
+                newPacketValues = JsonSerializer.Deserialize<JsonElement>("[" + leftPair.GetInt32() + "]");
+                return Compare(newPacketValues, rightPair);
+            //First item is list 2nd item is int
+            case (JsonValueKind.Array, JsonValueKind.Number):
+                newPacketValues = JsonSerializer.Deserialize<JsonElement>("[" + rightPair.GetInt32() + "]");
+                return Compare(leftPair, newPacketValues);
+            //Both items are lists
+            case (JsonValueKind.Array, JsonValueKind.Array):
+                var leftArray = leftPair.EnumerateArray();
+                var rightArray = rightPair.EnumerateArray();
+
+                while (leftArray.MoveNext() && rightArray.MoveNext())
+                {
+                    var leftPacketValues = leftArray.Current;
+                    var rightPacketValues = rightArray.Current;
+
+                    var compare = Compare(leftPacketValues, rightPacketValues);
+
+                    if (compare != 0)
+                    {
+                        return compare;
+                    }
+                }
+
+                var currentLeft = leftArray.Count() - rightArray.Count();
+                if(currentLeft == 0)
+                {
+                    return 0;
+                }
+                else if (currentLeft < 1)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            default:
+                return 0;
+        };
+    }
+
+   
+
+   
+    public int Compare(PacketValues pairs)
+    {
+        return Compare(packets, pairs.packets);
+    }
+
+    public static void Sort(List<PacketValues> sorted)
+    {
+        sorted.Sort(Comparer);
+    }
+
+    public static int Comparer(PacketValues x, PacketValues y)
+    {
+        return x.Compare(y);
+    }
+}
---
+++ AdventOfCodeDay13/Task1.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using AdventOfCodeDay13;
+
+List<PacketValues> leftPacketValues = new List<PacketValues>();
+List<PacketValues> rightPacketValues = new List<PacketValues>();
+int sum = 0;
+PacketValues.CreateList("C:\\Users\\BartłomiejWalczak\\source\\repos\\AdventOfCodeDay13\\AdventOfCodeDay13\\Day13Input.data", leftPacketValues, rightPacketValues);
+
+var leftArray = leftPacketValues.GetEnumerator();
+var rightArray = rightPacketValues.GetEnumerator();
+
+
+while (leftArray.MoveNext() && rightArray.MoveNext())
+{
+    var currentleft = leftArray.Current;
+    var currentright = rightArray.Current;
+
+    if (currentleft.Compare(currentright) < 0)
+    {
+        sum += currentleft.index;
+    }
+}
+Console.WriteLine(sum);
+
