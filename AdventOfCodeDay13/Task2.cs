42bdcd3308c9c6496653f6224ffb0b3a3477d3c0
vbawalc <73381454+vbawalc@users.noreply.github.com>
2022-12-15 20:30:18 +0100

    Add files via upload

---
+++ AdventOfCodeDay13/Task2.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using AdventOfCodeDay13;
+
+List<PacketValues> leftPacketValues = new List<PacketValues>();
+List<PacketValues> rightPacketValues = new List<PacketValues>();
+List<PacketValues> sortedPacketValues = new List<PacketValues>();
+int product = 0;
+int divisor = 1;
+int i = 1;
+var two = JsonSerializer.Deserialize<JsonElement>("[[2]]");
+var six = JsonSerializer.Deserialize<JsonElement>("[[6]]");
+
+
+PacketValues.CreateList("C:\\Users\\BartłomiejWalczak\\source\\repos\\AdventOfCodeDay13\\AdventOfCodeDay13\\Day13Input.data", sortedPacketValues, sortedPacketValues);
+sortedPacketValues.Add(new PacketValues(2, two));
+sortedPacketValues.Add(new PacketValues(4, six));
+PacketValues.Sort(sortedPacketValues);
+
+var sortedArray = sortedPacketValues.GetEnumerator();
+
+while (sortedArray.MoveNext())
+{
+    var sorted = sortedArray.Current;
+
+    product += sorted.Index;
+
+    if (sorted.Compare(two, sorted.packets) == 0 || sorted.Compare(six, sorted.packets) == 0)
+    {
+        divisor *= i;
+    }
+
+    i++;
+}
+Console.WriteLine(divisor);
